b71494133ef688dc27e29ecef2e857deb9796637
galtashma <home@Homes-iMac.local>
2015-01-11 00:49:01 +0200

    HACompiler is work in progress
    HACompiler will join the picture-baker and the compiler to be able to
    make quinable ascii js art in a click

---
+++ hacompiler.js
@@ -0,0 +1,16 @@
+function HACompiler(huskyCompiler, pictureBaker){
+    this.huskyCompiler = huskyCompiler;
+    this.pictureBaker = pictureBaker;
+}
+
+HACompiler.prototype.Compile = function(jscode) {  
+    var pic_dictionary = this.pictureBaker.BakePartialPicture(compiler.getDictionaryAsString());
+    var finalQuinableCode = compiler.CompileQuineableHusky(javascript, "quine", pic_dictionary.split('\n').join('\\\\n'));
+    var finalQuinablePic = pic_baker.BakePicture(finalQuinableCode);
+
+    // TODO: add /*\ to the end of the quine lines
+    //       add */ to the beginning of the lines
+    //       add "  " to beginning of all lines
+
+    eval(finalQuinablePic);
+};
